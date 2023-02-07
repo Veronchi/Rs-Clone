1246dabf0f7fc0a2a4f87d8472468621a2e2edc0
Sanek1n <sanekin8ru@outlook.com>
2023-02-07 13:31:37 +0400

    feat: implemented the display of boards

---
+++ src/pages/BoardPage/BoardPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BoardPage(): JSX.Element | null {
+  return (
+    <section className="board">
+      <h1 className="board__title">Board</h1>
+    </section>
+  );
+}
+
+export { BoardPage };
--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -1,19 +1,49 @@
 import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import { ModalWindow } from '../../components/ModalWindow/ModalWindow';
 import './style.scss';
+
+const boardArray = [
+  {
+    id: 1,
+    name: 'Moya borda',
+    color: '#CC9966',
+  },
+  {
+    id: 2,
+    name: 'Our Trello',
+    color: '#998877',
+  }];
 
 function BoardsPage(): JSX.Element | null {
   const [isModal, setIsModal] = useState<boolean>(false);
 
   const handleModalClose = (): void => setIsModal(false);
 
+  const navigate = useNavigate();
+
   return (
     <section className="boards">
       <h1 className="boards__title">Boards</h1>
       <div className="wrapper">
-        <button className="generate-btn" type="button" onClick={(): void => setIsModal(true)}>create board</button>
-
+        <div className="boards__buttons">
+          <button className="generate-btn" type="button" onClick={(): void => setIsModal(true)}>create board</button>
+          {
+            boardArray.map((element): JSX.Element => (
+              <button
+                type="button"
+                key={element.id}
+                className="boards__item"
+                style={{ backgroundColor: element.color }}
+                onClick={(): void => navigate('/board')}
+              >
+                <span className="boards__item-title">
+                  {element.name}
+                </span>
+              </button>
+            ))
+          }
+        </div>
       </div>
 
       {isModal ? <ModalWindow show={isModal} handleModal={handleModalClose} /> : null}
