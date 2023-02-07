4adad9c870ad5fcac4a3ada17e9b74e9bddf2dd2
Veronchi <zhigunova1995@gmail.com>
2023-02-07 18:49:09 +0300

    fix: project settings

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,57 +1,55 @@
 import React from 'react';
 import './Footer.scss';
 
-function Footer(): JSX.Element {
-  return (
-    <footer className="footer">
-      <div className="footer__container">
-        <div className="footer__left-container">
-          <a
-            href="https://github.com/Veronchi"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="footer__left-img"
-              src="https://fabulous-kitten-6e5e49.netlify.app/assets/img/github-logo.svg"
-              alt="github_logo"
-              title="Veronika"
-            />
-          </a>
-          <a href="https://github.com/Sanek1n" target="_blank" rel="noreferrer">
-            <img
-              className="footer__left-img"
-              src="https://fabulous-kitten-6e5e49.netlify.app/assets/img/github-logo.svg"
-              alt="github_logo"
-              title="Aleksandr"
-            />
-          </a>
-          <a
-            href="https://github.com/Satancrew"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              className="footer__left-img"
-              src="https://fabulous-kitten-6e5e49.netlify.app/assets/img/github-logo.svg"
-              alt="github_logo"
-              title="Vladimir"
-            />
-          </a>
-        </div>
-        <div className="footer__central-container">&copy; 2023</div>
-        <div className="footer__right-container">
-          <a href="https://rs.school/" target="_blank" rel="noreferrer">
-            <img
-              className="footer__right-img"
-              src="https://rs.school/images/rs_school_js.svg"
-              alt="github_logo"
-            />
-          </a>
-        </div>
+const Footer = (): JSX.Element => (
+  <footer className="footer">
+    <div className="footer__container">
+      <div className="footer__left-container">
+        <a
+          href="https://github.com/Veronchi"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="footer__left-img"
+            src="https://fabulous-kitten-6e5e49.netlify.app/assets/img/github-logo.svg"
+            alt="github_logo"
+            title="Veronika"
+          />
+        </a>
+        <a href="https://github.com/Sanek1n" target="_blank" rel="noreferrer">
+          <img
+            className="footer__left-img"
+            src="https://fabulous-kitten-6e5e49.netlify.app/assets/img/github-logo.svg"
+            alt="github_logo"
+            title="Aleksandr"
+          />
+        </a>
+        <a
+          href="https://github.com/Satancrew"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="footer__left-img"
+            src="https://fabulous-kitten-6e5e49.netlify.app/assets/img/github-logo.svg"
+            alt="github_logo"
+            title="Vladimir"
+          />
+        </a>
       </div>
-    </footer>
-  );
-}
+      <div className="footer__central-container">&copy; 2023</div>
+      <div className="footer__right-container">
+        <a href="https://rs.school/" target="_blank" rel="noreferrer">
+          <img
+            className="footer__right-img"
+            src="https://rs.school/images/rs_school_js.svg"
+            alt="github_logo"
+          />
+        </a>
+      </div>
+    </div>
+  </footer>
+);
 
 export default Footer;
--- src/components/HeaderSearch/HeaderSearch.tsx
+++ src/components/HeaderSearch/HeaderSearch.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import './HeaderSearch.scss';
 
-function HeaderSearch(): JSX.Element {
-  return (
-    <Form className="search-form">
-      <Form.Control className="search-form__input" type="text" placeholder="Search" />
-    </Form>
+const HeaderSearch = (): JSX.Element => (
+  <Form className="search-form">
+    <Form.Control className="search-form__input" type="text" placeholder="Search" />
+  </Form>
 
-  );
-}
+);
 
 export default HeaderSearch;
