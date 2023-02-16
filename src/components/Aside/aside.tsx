fbdf3667130ad3e5d68a15e547ad1116bd269042
Sanek1n <sanekin8ru@outlook.com>
2023-02-16 15:52:34 +0400

    feat: implemented create board from templates

--- src/components/Aside/aside.tsx
+++ src/components/Aside/aside.tsx
@@ -1,21 +1,57 @@
 import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { IState } from '../../interfaces';
+import { createBoard, getAllBoards } from '../../http/boardAPI';
+import { createCard } from '../../http/cardAPI';
+import { IBoard, IState } from '../../interfaces';
 import './style.scss';
+
+type NameTempl = 'minimal' | 'medium' | 'large';
+
+const template = {
+  minimal: {
+    title: 'Minimal board',
+    background: '#009900',
+    cards: ['To do', 'Done'],
+  },
+  medium: {
+    title: 'Medium board',
+    background: '#000099',
+    cards: ['To do', 'Doing', 'Done'],
+  },
+  large: {
+    title: 'Large board',
+    background: '#990000',
+    cards: ['Backlog', 'To do', 'Doing', 'Done'],
+  },
+};
 
 const Aside = (): JSX.Element => {
   const user = useSelector((state: IState) => state.user);
+  const boards = useSelector((state: IState) => state.boards);
   const [isShow, setIsShow] = useState<boolean>(true);
+  const dispatch = useDispatch();
 
   const handleAside = (): void => {
     setIsShow(!isShow);
   };
 
-  const handleTemplates = (): void => {
-    setIsShow(!isShow);
+  const handleTemplates = (templateName: NameTempl): void => {
+    const tempBoard = template[templateName];
+    createBoard(tempBoard.title, tempBoard.background)
+      .then((result: IBoard) => {
+        tempBoard.cards.forEach(async (element):Promise<void> => {
+          await createCard(element, result.id);
+        });
+      });
+    // .then(
+    //   dispatch(clean());
+    //   await getAllBoards()
+    //   .then((data) => {
+    //     dispatch(addBoards([data]));
+    //   }));
   };
 
   return (
@@ -37,9 +73,9 @@ const Aside = (): JSX.Element => {
                 <Link to="/boards" className="aside__boards">Boards</Link>
                 <Link to="/auth" className="aside__users" onClick={(): void => localStorage.removeItem('token')}>Users</Link>
                 <NavDropdown title="Templates" id="basic-nav-dropdown" className="aside__settings">
-                  <NavDropdown.Item onClick={handleTemplates} className="aside__theme">Minimal (with 2 cards)</NavDropdown.Item>
-                  <NavDropdown.Item onClick={handleTemplates} className="aside__theme">Medium (with 3 cards)</NavDropdown.Item>
-                  <NavDropdown.Item onClick={handleTemplates} className="aside__lang">Large (with 4 cards)</NavDropdown.Item>
+                  <NavDropdown.Item onClick={(): void => handleTemplates('minimal')} className="aside__theme">Minimal (with 2 cards)</NavDropdown.Item>
+                  <NavDropdown.Item onClick={(): void => handleTemplates('medium')} className="aside__theme">Medium (with 3 cards)</NavDropdown.Item>
+                  <NavDropdown.Item onClick={(): void => handleTemplates('large')} className="aside__lang">Large (with 4 cards)</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Settings" id="basic-nav-dropdown" className="aside__settings">
                   <NavDropdown.Item href="#" className="aside__theme">Themes</NavDropdown.Item>
