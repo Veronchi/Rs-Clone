c86119417be39eba497a0282397138a9ddfbf24c
Vladimir Orlov <satancrew.true@gmail.com>
2023-02-21 13:46:11 +0300

    fix: change buttons card task

--- src/components/CardTask/CardTask.tsx
+++ src/components/CardTask/CardTask.tsx
@@ -37,22 +37,23 @@ const CardTask: FC<ITaskProps> = ({ task }): JSX.Element => {
 
   return (
     <li className="tasks__item" draggable onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-      <div className="task-header" style={{ backgroundColor: '#ccccff' }} />
-      <div className="task-item">
-        {taskTitle}
+      <div className="task-header" style={{ backgroundColor: '#ccccff' }}>
         {isHover
           ? (
             <Dropdown>
-              <Dropdown.Toggle className="task-item__btn">
+              <Dropdown.Toggle className="task-header__btn">
                 <i className="bx bx-dots-horizontal-rounded bx-xs" />
               </Dropdown.Toggle>
-              <Dropdown.Menu className="task-item__menu">
-                <Dropdown.Item className="task-item__link" onClick={handleClick}>Edit task</Dropdown.Item>
-                <Dropdown.Item className="task-item__link" onClick={(): void => deleteTask(task.id)}> Delete task</Dropdown.Item>
+              <Dropdown.Menu className="task-header__menu">
+                <Dropdown.Item className="task-header__link" onClick={handleClick}>Edit task</Dropdown.Item>
+                <Dropdown.Item className="task-header__link" onClick={(): void => deleteTask(task.id)}> Delete task</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           )
           : null}
+      </div>
+      <div className="task-item">
+        {taskTitle}
         {isActive
           ? <input onChange={handleTitle} onKeyDown={handleKeyDown} placeholder={taskTitle} className="task-item__input" type="text" />
           : null }
