4db44d0dec0f7685c292da7f3d363aea379c5c82
Vladimir Orlov <satancrew.true@gmail.com>
2023-02-22 15:38:55 +0300

    fix: change styles for welcome page

--- src/components/AboutDevelopers/AboutDevelopers.tsx
+++ src/components/AboutDevelopers/AboutDevelopers.tsx
@@ -10,24 +10,40 @@ const AboutDevelopers = (): JSX.Element => (
     <div className="welcome__cards">
       <div>
         <h2 className="welcome__cards-name">Alexander</h2>
-        <div className="welcome__cards-info">
-          <img alt="alexander" src={alexander} className="welcome__cards-photo" />
-          <p className="welcome__cards-job">Front-End Developer</p>
-        </div>
+        <a href="https://github.com/Sanek1n" target="_blank" rel="noreferrer" className="welcome__cards-link">
+          <div className="welcome__cards-info">
+            <img alt="alexander" src={alexander} className="welcome__cards-photo" />
+            <p className="welcome__cards-job">Front-End Developer</p>
+          </div>
+        </a>
       </div>
       <div>
         <h2 className="welcome__cards-name">Vladimir</h2>
-        <div className="welcome__cards-info">
-          <img src={vladimir} alt="vladimir" className="welcome__cards-photo" />
-          <p className="welcome__cards-job">Front-End Developer</p>
-        </div>
+        <a
+          href="https://github.com/Satancrew"
+          target="_blank"
+          rel="noreferrer"
+          className="welcome__cards-link"
+        >
+          <div className="welcome__cards-info">
+            <img src={vladimir} alt="vladimir" className="welcome__cards-photo" />
+            <p className="welcome__cards-job">Front-End Developer</p>
+          </div>
+        </a>
       </div>
       <div>
         <h2 className="welcome__cards-name">Veronica</h2>
-        <div className="welcome__cards-info">
-          <img src={veronica} alt="veronica" className="welcome__cards-photo" />
-          <p className="welcome__cards-job team-lead">Team Lead</p>
-        </div>
+        <a
+          href="https://github.com/Veronchi"
+          target="_blank"
+          rel="noreferrer"
+          className="welcome__cards-link"
+        >
+          <div className="welcome__cards-info">
+            <img src={veronica} alt="veronica" className="welcome__cards-photo" />
+            <p className="welcome__cards-job team-lead">Team Lead</p>
+          </div>
+        </a>
       </div>
     </div>
   </div>
